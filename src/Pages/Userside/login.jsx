2674cf6c642f23d732c8b10ebfb5789f2049685e
kainat robi <kainatrobi01@gmail.com>
2024-09-18 23:51:27 -0700

    chnages

--- src/Pages/Userside/login.jsx
+++ src/Pages/Userside/login.jsx
@@ -4,17 +4,17 @@ const Login = () => {
   return (
     <div className="flex flex-row h-screen">
       {/* Sidebar with fixed width */}
-      <div>
+      <div className="w-[45%]">
       <Leftsidebar />
-
       </div>
 
       {/* Main Content Area */}
       <div className=" w-[130vh] bg-[#f8f8f8] flex items-center justify-center">
-        <div className=" shadow-lg rounded-lg p-8 max-w-md w-full text-center">
+        <div className="  p-8 max-w-md w-full text-center">
           <h1 className="text-2xl font-bold mb-6 text-gray-800">Welcome Back</h1>
-          {/* Additional content (like forms) can be added here */}
-        </div>
+          <p>Lorem Ipsum ha sido el texto de relleno estándar</p>
+
+         </div>
       </div>
     </div>
   );
