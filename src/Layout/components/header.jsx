efb49f255e6ac5e8a45dc97e7710631a1acc228a
amirsuliman01 <amirsuliman01@gmail.com>
2024-09-20 18:12:58 +0500

    layout corrected

--- src/Layout/components/header.jsx
+++ src/Layout/components/header.jsx
@@ -1,21 +1,23 @@
-import { ChevronDown, Search } from "lucide-react";
+import { ChevronDown, Search } from 'lucide-react';
 
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 flex w-full ">
       <div className="flex flex-grow items-center justify-between px-4 py-4 md:px-6 2xl:px-11 ">
         {/* Left: Welcome Section */}
-        <div className="flex flex-col">
-          <h1 className="text-[20px] font-medium text-black">Welcome back, Bocil Prik</h1>
+        <div className="flex flex-col w-full">
+          <h1 className="text-[20px] font-medium text-black">
+            Welcome back, Bocil Prik
+          </h1>
           <p className="text-sm text-gray-400">22 September 2022 | 10:45 AM</p>
         </div>
 
         {/* Right: Search, Notifications, Profile */}
-        <div className="flex items-center justify-end gap-4 ">
+        <div className="flex items-center justify-end gap-4 w-full">
           {/* Search Bar */}
-          <div className="relative hidden  sm:flex items-center rounded-full bg-white px-4 py-3 w-[700px]">
+          <div className="relative hidden  sm:flex items-center rounded-full bg-white px-4 py-3 w-full">
             <input
-              type="text"
+              type="search"
               placeholder="Search..."
               className="w-full bg-transparent text-black focus:outline-none"
             />
@@ -24,12 +26,20 @@ const Header = () => {
 
           {/* Notification Icon */}
           <div className="bg-white rounded-full p-3 shadow-md">
-            <img src="/bell-01.png" alt="Notification Icon" className="h-6 w-6" />
+            <img
+              src="/bell-01.png"
+              alt="Notification Icon"
+              className="h-6 w-6"
+            />
           </div>
 
           {/* Profile Avatar */}
           <div className="bg-[#FDC546] rounded-full p-1 shadow-md">
-            <img src="/Memoji Boys 6-18.png" alt="Profile Avatar" className="h-10 w-10" />
+            <img
+              src="/Memoji Boys 6-18.png"
+              alt="Profile Avatar"
+              className="h-10 w-10"
+            />
           </div>
 
           {/* Dropdown Icon */}
