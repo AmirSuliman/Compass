eaa748ce88754f1fd33db090ed817e4b3012fe08
amirsuliman01 <amirsuliman01@gmail.com>
2024-09-27 17:06:57 +0500

    all branches merged

--- src/routes/main.routes.jsx
+++ src/routes/main.routes.jsx
@@ -3,21 +3,12 @@ import App from '../App';
 import { createBrowserRouter } from 'react-router-dom';
 import usersideRoutes from './userside.routes';
 import adminsideRoutes from './adminside.routes';
-import landingPageRoutes from './landingpage.routes';  // Import landing page routes
 
 const mainRoutes = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [
-      // ...usersideRoutes.routes,
-      // ...landingPageRoutes.routes,  // Add the landing page routes here
-      // ...candidateRoutes.routes,
-      // ...employeeRouter.routes,
-      ...adminsideRoutes.routes,
-      // ...landingPageRouter,
-      // ...adminRouter.routes,
-    ],
+    children: [...usersideRoutes.routes, ...adminsideRoutes.routes],
   },
 ]);
 
