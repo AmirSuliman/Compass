e5e457e8a5b99185b6a1b55e71a1ec0ec445d5b0
amirsuliman01 <amirsuliman01@gmail.com>
2024-09-30 11:38:33 +0500

    deployment issue resolved

--- src/routes/main.routes.jsx
+++ src/routes/main.routes.jsx
@@ -3,12 +3,11 @@ import App from '../App';
 import { createBrowserRouter } from 'react-router-dom';
 import usersideRoutes from './userside.routes';
 import adminsideRoutes from './adminside.routes';
-import landingPageRoutes from './landingpage.routes';
 const mainRoutes = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [...usersideRoutes.routes, ...adminsideRoutes.routes, ...landingPageRoutes.routes],
+    children: [...usersideRoutes.routes, ...adminsideRoutes.routes],
   },
 ]);
 
